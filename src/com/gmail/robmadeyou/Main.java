17bac16e93616215d9e7c2b3a5751a61ad7b07d2
robmadeyou <robmadeyou@gmail.com>
2014-11-11 23:39:29 +0000

    Moved everything again to a non-source directory. Added some basic view so it suits the logic of Abereth!

--- src/com/gmail/robmadeyou/Main.java
+++ src/com/gmail/robmadeyou/Main.java
@@ -1,7 +1,18 @@
 package com.gmail.robmadeyou;
 
+import com.abereth.game.Game;
+import com.abereth.tests.trianges.TriangleTests;
+
 /**
  * Created by jeremiah on 11/11/2014.
  */
 public class Main {
+
+	public static void main ( String[] args )
+	{
+		Game g = new Game( 1024, 512 );
+		g.setTaskBarIcon( "res\\icon16.png", "res\\icon32.png" );
+		g.addView ( new TriangleTests( g ) );
+		g.start();
+	}
 }
---
+++ src/com/gmail/robmadeyou/views/IntroLoading.java
@@ -0,0 +1,21 @@
+package com.gmail.robmadeyou.views;
+
+import com.abereth.game.Game;
+import com.abereth.game.View;
+
+/**
+ * Created by jeremiah on 11/11/2014.
+ */
+public class IntroLoading extends View
+{
+	public IntroLoading(Game game)
+	{
+		super(game);
+	}
+
+	@Override
+	public void update(int delta)
+	{
+
+	}
+}
