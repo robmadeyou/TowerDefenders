a7ebba73c27956eda375f572a46e29ac7a263d37
Robert Ellis <robmadeyou@gmail.com>
2014-12-10 14:04:05 +0000

    Added a temporary physics test using the Dyn4j library

--- src/com/gmail/robmadeyou/Main.java
+++ src/com/gmail/robmadeyou/Main.java
@@ -3,20 +3,22 @@ package com.gmail.robmadeyou;
 import com.abereth.draw.Color;
 import com.abereth.game.Game;
 import com.abereth.tests.trianges.TriangleTests;
+import com.gmail.robmadeyou.views.IntroLoading;
 
 /**
  * Created by jeremiah on 11/11/2014.
  */
-public class Main {
+public class Main
+{
 
 	public static void main ( String[] args )
 	{
 		Game g = new Game( 1024, 512 );
 		g.setTaskBarIcon( "res\\icon16.png", "res\\icon32.png" );
-		TriangleTests view = new TriangleTests( g );
+		IntroLoading view = new IntroLoading( g );
 		view.VIEW_COLOR = Color.BISQUE_2;
 		view.fadeIn( 1 );
 		g.addView ( view );
-		g.start();
+		g.Start();
 	}
 }
--- src/com/gmail/robmadeyou/views/IntroLoading.java
+++ src/com/gmail/robmadeyou/views/IntroLoading.java
@@ -1,7 +1,15 @@
 package com.gmail.robmadeyou.views;
 
+import com.abereth.G;
 import com.abereth.game.Game;
 import com.abereth.game.View;
+import com.abereth.input.Mouse;
+import org.dyn4j.dynamics.Body;
+import org.dyn4j.dynamics.BodyFixture;
+import org.dyn4j.dynamics.World;
+import org.dyn4j.geometry.*;
+
+import static org.lwjgl.opengl.GL11.*;
 
 /**
  * Created by jeremiah on 11/11/2014.
@@ -12,10 +20,109 @@ public class IntroLoading extends View
 	{
 		super(game);
 	}
+	private long last = 0;
+	public static final double NANO_TO_BASE = 1.0e9;
+	public World world;
+	@Override
+	public void Initialize()
+	{
+		super.Initialize();
+		this.world = new World();
+
+		// create all your bodies/joints
+
+		// create the floor
+		Rectangle floorRect = new Rectangle(40, 0.5);
+		Body floor = new Body();
+		floor.addFixture(new BodyFixture(floorRect));
+		floor.setMass( Mass.Type.INFINITE );
+		// move the floor down a bit
+		floor.translate(2, -10);
+		this.world.addBody(floor);
+
+
+		/*Body issTri = new Body();
+		issTri.addFixture(Geometry.createIsoscelesTriangle(1.0, 3.0));
+		issTri.setMass();
+		issTri.translate(0.2, 3.0);
+		this.world.addBody(issTri);
+
+		Body equTri = new Body();
+		equTri.addFixture(Geometry.createEquilateralTriangle(2.0));
+		equTri.setMass();
+		equTri.translate(0.2, 3.0);
+		this.world.addBody(equTri);*/
+
+		Body rightTri = new Body();
+		rightTri.addFixture(Geometry.createRightTriangle(2.0, 1.0));
+		rightTri.setMassType( Mass.Type.INFINITE );
+		rightTri.setMass();
+		rightTri.translate( 1, 0);
+		this.world.addBody(rightTri);
+
+		Body rectangle = new Body();
+		rectangle.addFixture( Geometry.createSquare( 2 ) );
+		rectangle.setMass();
+		Transform t = new Transform();
+		t.setRotation( 2 );
+		rectangle.setTransform( t );
+		// move the floor down a bit
+		this.world.addBody(rectangle);
+	}
 
 	@Override
 	public void update(int delta)
 	{
 
+		if( Mouse.isLeftMouseDown() )
+		{
+			Body rightTri = new Body();
+			rightTri.addFixture(Geometry.createRightTriangle( 2.0, 1.0 ));
+			rightTri.setMassType( Mass.Type.INFINITE );
+			rightTri.setMass();
+			rightTri.translate( Mouse.getX() / 45, -Mouse.getY() / 45 );
+			this.world.addBody(rightTri);
+		}
+		else if( Mouse.isRightMouseDown() )
+		{
+			Body rectangle = new Body();
+			rectangle.addFixture( Geometry.createSquare( 2 ) );
+			rectangle.setMass();
+			Transform t = new Transform();
+			t.setRotation( 2 );
+			rectangle.setTransform( t );
+			rectangle.translate( Mouse.getX() / 45, -Mouse.getY() / 45 );
+			// move the floor down a bit
+			this.world.addBody(rectangle);
+		}
+		for (int i = 0; i < this.world.getBodyCount(); i++) {
+			glPushMatrix();
+			glScaled( 45, 45, 45 );
+			// transform the coordinate system from world coordinates to local coordinates
+			glTranslated(this.world.getBody(i).getTransform().getTranslationX(), -this.world.getBody(i).getTransform().getTranslationY(), 0.0);
+			// rotate about the z-axis
+			glRotated( -Math.toDegrees( this.world.getBody( i ).getTransform().getRotation() ), 0.0, 0.0, 1.0 );
+			System.out.println( Math.toDegrees( this.world.getBody( i ).getTransform().getRotation() ) );
+			// get the object
+			Body go = this.world.getBody(i);
+			Polygon p = ( Polygon ) go.getFixture( 0 ).getShape();
+			glColor3d( Math.random(), Math.random(), Math.random() );
+			glBegin( GL_POLYGON );
+				for( Vector2 v : p.getVertices() )
+				{
+					glVertex2d( v.x, -v.y );
+				}
+			glEnd();
+			glPopMatrix();
+		}
+		long time = System.nanoTime();
+		// get the elapsed time from the last iteration
+		long diff = time - this.last;
+		// set the last time
+		this.last = time;
+		// convert from nanoseconds to seconds
+		double elapsedTime = (double)diff / NANO_TO_BASE;
+		// update the world with the elapsed time
+		this.world.update(elapsedTime);
 	}
 }
